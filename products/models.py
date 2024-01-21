212ab15712fba0f29a5f7764c3a4acb92995edea
Roman Kopylov <romankopilov1712@gmail.com>
2024-01-21 09:28:04 +0300

    add products and categories to db, works with media files

--- products/models.py
+++ products/models.py
@@ -2,11 +2,15 @@ from django.db import models
 
 # Create your models here.
 
+
 class ProductsCategory(models.Model):
     # зачем мы здесь перед классами CharField и тд добавляем скобки?
     # где поле id? primary_key=True сделает поле по умолчанию id(по дефолту джанго сам создает столбец id)
     name = models.CharField(max_length=128) # что такое max_length? что такое CharField?
     description = models.TextField(null=True, blank=True) # В чем отличие от CharField? для чего добавлять null=True, blank=True? (чтобы это строка могла быть пустой в таблице)
+    def __str__(self):
+        return self.name
+
 
 class Product(models.Model):
     name = models.CharField(max_length=256)
@@ -16,3 +20,6 @@ class Product(models.Model):
     image = models.ImageField(upload_to='products_images') # папка, где сохраняем картинки
     # связываем с таблицей ProductsCategory
     category = models.ForeignKey(to=ProductsCategory, on_delete=models.CASCADE) # класс CASCADE не вызывается. При CASCADE удаляются связанные категории. Еще есть Set_default/ PROTECT. Что за ForeignKey, to=ProductsCategory?
+
+    def __str__(self):
+        return f'Товар:{self.name} из категории: {self.category.name}'
