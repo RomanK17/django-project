96415dc999c619a8ec1952a902dac23cffb0cb9f
Roman Kopylov <romankopilov1712@gmail.com>
2024-02-11 12:20:27 +0300

    fixed login & registraation

--- users/views.py
+++ users/views.py
@@ -1,9 +1,23 @@
 from django.shortcuts import render
+from django.contrib import auth
+
+from users.form import UserLoginForm
 
 # Create your views here.
 
 def log(request):
-    return render(request, 'users/login.html')
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(username=username, password=password)
+            if user:
+                auth.login(request,user)
+    else:
+        form = UserLoginForm()
+    context = {'form': form}
+    return render(request, 'users/login.html', context)
 
 def reg(request):
     return render(request, 'users/register.html')
