76600bc166d9ade210cd0a61dfb2f208332a57ca
Roman Kopylov <romankopilov1712@gmail.com>
2024-02-12 23:21:30 +0300

    fixed bug with wrong path for registration + add messages for user

--- users/views.py
+++ users/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, HttpResponseRedirect
-from django.contrib import auth
+from django.contrib import auth, messages
 from django.urls import reverse
 
 from users.form import UserLoginForm, UserRegistrationForm, UserProfileForm
@@ -26,6 +26,7 @@ def reg(request):
         form = UserRegistrationForm(data=request.POST)
         if form.is_valid():
             form.save()
+            messages.success(request, 'Вы зарегистрированы!!!')
             return HttpResponseRedirect(reverse('users:login'))
     else:
         form = UserRegistrationForm()
